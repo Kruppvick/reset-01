6f15e2c4aab6cb3a9218098e601583e3d994e7c2
Kruppvick <vrpraupp@gmail.com>
2020-03-07 12:13:07 -0300

    Removendo mensagens estranhas

--- src/Início.java
+++ src/Início.java
@@ -2,10 +2,7 @@ public class Início {
 
     public static void main (String[] args){
         System.out.println("Hello World");
-        System.out.println("Consegui");
-        System.out.println("fEITOOO");
-        System.out.println("Obah");
-        System.out.println("Chuchu beleza");
+   
 
     }
 
