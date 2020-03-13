19ddd046e1fc2bd7576415b5ec6b9f60f1ec6bfb
Kruppvick <61908816+Kruppvick@users.noreply.github.com>
2020-03-12 22:52:51 -0300

    Add files via upload

---
+++ src/PoderDivino.java
@@ -0,0 +1,32 @@
+public class PoderDivino {
+
+    String nome;
+    int poderesDivinos;
+    int custoFe;
+
+
+    public PoderDivino(String nome, int poderesDivinos) {
+        this.nome = nome;
+        this.poderesDivinos = poderesDivinos;
+        this.custoFe= custoFe;
+
+    }
+    void atacar (Barbaro barbaro, Arma arma){
+    }
+
+    void atacar(Clerigo clerigo, PoderDivino poderDivino){
+
+    }
+    void atacar(Druida druida, PoderDivino poderDivino){
+
+    }
+
+    void atacar(Feiticeiro feiticeiro, Magia magia){
+
+    }
+    void atacar(Guerreiro guerreiro, Arma arma){
+
+    }
+    void atacar(Mago mago, Magia magia) {
+    }
+}
